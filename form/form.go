f3bd11d9b2a468cd3a3e80c9565fa462970ab99a
MarySmirnova <marysmirnova_work@mail.ru>
2022-01-27 22:47:34 +1000

    add logging and creating a form in 2 threads

--- form/form.go
+++ form/form.go
@@ -1,9 +1,15 @@
 package form
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type DataToFill struct {
 	Pages []Page
+
+	m    sync.Mutex
+	form map[string]interface{}
 }
 
 type Page struct {
@@ -11,29 +17,36 @@ type Page struct {
 	Name       string
 	ID         string
 	Box        int
-	Rows       []Row
+	Rows       [][]interface{}
 }
 
-type Row struct {
-	Columns []interface{}
-}
+func (d *DataToFill) CreateForm() (map[string]interface{}, error) {
+	var wg sync.WaitGroup
+	d.form = make(map[string]interface{})
 
-func (d DataToFill) CreateForm() (map[string]interface{}, error) {
-	form := make(map[string]interface{})
 	if len(d.Pages) > 2 {
 		return nil, fmt.Errorf("pages cannot be more than 2")
 	}
 
 	for i := range d.Pages {
-		err := d.Pages[i].createPage(form)
+		wg.Add(1)
+		var err error
+		go func(i int, form map[string]interface{}, m *sync.Mutex) {
+			defer wg.Done()
+			err = d.Pages[i].createPage(form, m)
+			if err != nil {
+				return
+			}
+		}(i, d.form, &d.m)
 		if err != nil {
 			return nil, err
 		}
 	}
-	return form, nil
+	wg.Wait()
+	return d.form, nil
 }
 
-func (p Page) createPage(form map[string]interface{}) error {
+func (p *Page) createPage(form map[string]interface{}, m *sync.Mutex) error {
 	if p.Box < 1 || p.Box > 3 {
 		return fmt.Errorf("the box parameter must take values from 1 to 3")
 	}
@@ -41,11 +54,12 @@ func (p Page) createPage(form map[string]interface{}) error {
 		return fmt.Errorf("rows cannot be more than 14")
 	}
 
-	dTotal, eTotal, gTotal, hTotal, err := p.createRows(form)
+	dTotal, eTotal, gTotal, hTotal, err := p.createRows(form, m)
 	if err != nil {
 		return err
 	}
 
+	m.Lock()
 	form[fmt.Sprintf("topmostSubform[0].Page%d[0].f%d_1[0]", p.PageNumber, p.PageNumber)] = p.Name
 	form[fmt.Sprintf("topmostSubform[0].Page%d[0].f%d_2[0]", p.PageNumber, p.PageNumber)] = p.ID
 	form[fmt.Sprintf("topmostSubform[0].Page%d[0].c%d_1[%d]", p.PageNumber, p.PageNumber, p.Box-1)] = p.Box
@@ -54,22 +68,26 @@ func (p Page) createPage(form map[string]interface{}) error {
 	form[fmt.Sprintf("topmostSubform[0].Page%d[0].f%d_116[0]", p.PageNumber, p.PageNumber)] = eTotal
 	form[fmt.Sprintf("topmostSubform[0].Page%d[0].f%d_118[0]", p.PageNumber, p.PageNumber)] = gTotal
 	form[fmt.Sprintf("topmostSubform[0].Page%d[0].f%d_119[0]", p.PageNumber, p.PageNumber)] = hTotal
+	m.Unlock()
+
 	return nil
 }
 
-func (p Page) createRows(form map[string]interface{}) (dTotal, eTotal, gTotal, hTotal float64, err error) {
+func (p *Page) createRows(form map[string]interface{}, m *sync.Mutex) (dTotal, eTotal, gTotal, hTotal float64, err error) {
 	id := 3
 	for i := range p.Rows {
-		if len(p.Rows[i].Columns) > 7 {
+		if len(p.Rows[i]) > 7 {
 			return 0, 0, 0, 0, fmt.Errorf("columns cannot be more than 7")
 		}
 		var gain float64
 		for j := 0; j < 7; j++ {
-			if len(p.Rows[i].Columns) > j {
-				form[fmt.Sprintf("topmostSubform[0].Page%d[0].Table_Line1[0].Row%d[0].f%d_%d[0]", p.PageNumber, i+1, p.PageNumber, id)] = p.Rows[i].Columns[j]
+			if len(p.Rows[i]) > j {
+				m.Lock()
+				form[fmt.Sprintf("topmostSubform[0].Page%d[0].Table_Line1[0].Row%d[0].f%d_%d[0]", p.PageNumber, i+1, p.PageNumber, id)] = p.Rows[i][j]
+				m.Unlock()
 
 				if (j > 2 && j < 5) || j == 6 {
-					val, ok := p.Rows[i].Columns[j].(float64)
+					val, ok := p.Rows[i][j].(float64)
 					if !ok {
 						return 0, 0, 0, 0, fmt.Errorf("costs should be float64")
 					}
@@ -88,7 +106,10 @@ func (p Page) createRows(form map[string]interface{}) (dTotal, eTotal, gTotal, h
 			}
 			id++
 		}
+		m.Lock()
 		form[fmt.Sprintf("topmostSubform[0].Page%d[0].Table_Line1[0].Row%d[0].f%d_%d[0]", p.PageNumber, i+1, p.PageNumber, id)] = gain
+		m.Unlock()
+
 		hTotal += gain
 		id++
 	}
--- main.go
+++ main.go
@@ -14,25 +14,37 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/fillpdf", completeDocument).Methods("POST")
+	r.HandleFunc("/fillpdf", completeDocument)
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func completeDocument(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "wrong method", 405)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	data := new(form.DataToFill)
 	err := json.NewDecoder(r.Body).Decode(data)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		http.Error(w, err.Error(), 400)
 		return
 	}
 	form, err := data.CreateForm()
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		http.Error(w, err.Error(), 400)
 		return
 	}
 	err = fillpdf.Fill(form, "f8949.pdf", fmt.Sprintf("data/%s.pdf", data.Pages[0].Name))
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
+
+	log.Printf("create file %s.pdf", data.Pages[0].Name)
+	w.Write([]byte("filling pdf file completed"))
 }
